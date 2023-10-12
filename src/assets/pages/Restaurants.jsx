b96db920c7bb1575fb329a4d66e3888d9b1415b0
SumithShetty1 <sumithshetty014@gmail.com>
2023-10-13 00:45:43 +0530

    Added Sort By and Updated CSS

--- src/assets/pages/Restaurants.jsx
+++ src/assets/pages/Restaurants.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
 import '../styles/Restaurants.css'
 import menuicon from "../images/menu-icon.png";
@@ -6,6 +6,8 @@ import locationicon from "../images/location-icon.png";
 import charcoals from "../images/Charcoals.png";
 
 function Restaurants({ selectedLocation }) {
+  const [sortOption, setSortOption] = useState('Popularity');
+
   const restaurantData = [
     { name: "Charcoal's Family Restaurant", location: "Moodbidri", ratings: 4.5 },
     { name: "Charcoal's Family Restaurant", location: "Moodbidri", ratings: 4.5 },
@@ -18,6 +20,13 @@ function Restaurants({ selectedLocation }) {
       <section className='recommended'>
         <div className='recommended-header'>
           <h1>Recommended restaurants in {selectedLocation}</h1>
+          <div className='sort-container'>
+            <label for="sort">Sort By</label>
+            <select id='sort' value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
+              <option value="popularity">Popularity</option>
+              <option value="ratings">Ratings</option>
+            </select>
+          </div>
         </div>
         <div className='recommended-container'>
           {restaurantData.map((restaurant, index) => (
@@ -27,20 +36,25 @@ function Restaurants({ selectedLocation }) {
               <div className='restaurant-details'>
                 <h2>{restaurant.name}</h2>
                 <p>{restaurant.location}</p>
-                <Link>
-                  <img src={menuicon} alt='' />
-                  View Menu
-                </Link>
-                <Link>
-                  <img src={locationicon} alt='' />
-                  See Location
-                </Link>
+                <span className='restaurant-links'>
+                  <Link>
+                    <img src={menuicon} alt='' />
+                    <span>View Menu</span>
+                  </Link>
+                  <Link>
+                    <img src={locationicon} alt='' />
+                    <span>See Location</span>
+                  </Link>
+                </span>
               </div>
               <button className='booktable-btn'>Book a Table</button>
             </section>
           ))}
         </div>
       </section>
+      <aside className='filter'>
+        
+      </aside>
     </main>
   );
 }
