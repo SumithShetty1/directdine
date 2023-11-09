e2af5d9f00b05eae2ec180ae7ccbef1dea91424c
SumithShetty1 <sumithshetty014@gmail.com>
2023-11-10 00:00:57 +0530

    Profile Icon

--- src/assets/components/Header/Login.jsx
+++ src/assets/components/Header/Login.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
-import {auth,provider,signInWithPopup} from "../../../firebaseConfig"
+import { auth, provider, signInWithPopup } from "../../../firebaseConfig"
 import profile from "../../images/profile.png"
 
 function Login() {
-    const signInWithGoogle = () =>{
+    const signInWithGoogle = () => {
         signInWithPopup(auth, provider)
 
-        .then((result) => {
-          // Handle the user authentication result here.
-          const user = result.user;
-          console.log('Successfully signed in:', user);
-        //   <img src={profile} alt="" className='login-profile' />
-        })
-        .catch((error) => {
-          // Handle errors, if any.
-          console.error('Error during sign-in:', error);
-        });
+            .then((result) => {
+                // Handle the user authentication result here.
+                const user = result.user;
+                console.log('Successfully signed in:', user);
+                //   <img src={profile} alt="" className='login-profile' />
+            })
+            .catch((error) => {
+                // Handle errors, if any.
+                console.error('Error during sign-in:', error);
+            });
     }
 
     return (
         <>
             {/* <button className="login-btn" onClick={signInWithGoogle}>Login</button> */}
-            <div className='profile-dropdown'>
-                <input type='checkbox' id="login-profile"/>
+            {/* <div className='profile-dropdown'>
+                <input type='checkbox' id="login-profile" />
                 <label htmlFor='login-profile' className='login-label'>
-                    <img src={profile} alt="" className='login-profile' />
+                    <div>
+                        <img src={profile} alt="" className='login-profile' />
+                    </div>
                 </label>
                 <div className='dropDownProfile'>
                     <ul>
@@ -33,7 +35,26 @@ function Login() {
                         <li>Logout</li>
                     </ul>
                 </div>
-            </div>
+            </div> */}
+            <ul>
+                <li>
+                    <img src={profile} className="profile" alt="Profile" />
+                    <ul>
+                        <li className="profile-name">
+                            <p>Sumith</p>
+                        </li>
+                        <li className="sub-item">
+                            <p>Bookings</p>
+                        </li>
+                        <li className="sub-item">
+                            <p>Reservations</p>
+                        </li>
+                        <li className="sub-item">
+                            <p>Logout</p>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </>
     );
 }
