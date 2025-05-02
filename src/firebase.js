48439125cf3d8144ae7d7d53fa8fd9ee9525e9ac
Sumith Shetty <sumithshetty014@gmail.com>
2025-05-02 18:12:54 +0530

    Added .env

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuUQ1ZUukJ46LzloWdHqRpKIZTVC85IVE",
-  authDomain: "directdine-d4261.firebaseapp.com",
-  projectId: "directdine-d4261",
-  storageBucket: "directdine-d4261.appspot.com",
-  messagingSenderId: "1003236394722",
-  appId: "1:1003236394722:web:5d3743bc4176a8222b00bb"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
